0fd7c21c067b6b5ef17c972c3a499a7bd1fe2f72
aryan <legto3@gmail.com>
2026-01-16 20:28:43 +0100

    typewriter

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,18 @@
+import Typewriter from "@/components/ui/typewriter";
+
 export default function Home() {
   return (
-    <div>
-      {/* Custom text animation component will go here */}
+    <div className="flex min-h-[70vh] items-center justify-center">
+    <p className="text-3xl font-bold sm:text-4xl">
+        <span>{"some dummy "}</span>
+        <Typewriter
+          text={["text", "huh", "text yes"]}
+          className="text-foreground"
+          waitTime={1000}
+          cursorChar={"_"}
+          loopDelay={2000}
+        />
+      </p>
     </div>
   );
 }
